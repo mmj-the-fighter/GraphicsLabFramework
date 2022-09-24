7b7548affb61d67a214f1fa1be8de2b788dbc2ec
Manoj M J <mmjthefighter@gmail.com>
2022-09-24 18:15:54 +0530

    update font rendering

--- swr_rasterizer.c
+++ swr_rasterizer.c
@@ -57,8 +57,7 @@ void rasterizer_draw_line_dda(int x0, int y0, int x1, int y1)
 	int dx = x1 - x0;
 	int dy = y1 - y0;
 
-	if (abs(dx) > abs(dy))
-	{
+	if (abs(dx) > abs(dy)) {
 		steps = abs(dx);
 		xincr = dx / (float)steps;
 		yincr = dy / (float)steps;
@@ -87,19 +86,25 @@ void rasterizer_draw_line_bres(int x0, int y0, int x1, int y1)
 	dx = x1 - x0;
 	dy = y1 - y0;
 
-	if (dx > 0)
+	if (dx > 0) {
 		s1 = 1;
-	else if (dx < 0)
+	}
+	else if (dx < 0) {
 		s1 = -1;
-	else
+	}
+	else {
 		s1 = 0;
+	}
 
-	if (dy > 0)
+	if (dy > 0) {
 		s2 = 1;
-	else if (dy < 0)
+	}
+	else if (dy < 0) {
 		s2 = -1;
-	else
+	}
+	else {
 		s2 = 0;
+	}
 		
 	x = x0;
 	y = y0;
@@ -194,20 +199,22 @@ void rasterizer_copy_pixels(
 
 	/* sanity check dst_start coordinates */
 	if (
-		(dst_x_start < 0 || dst_x_start >= pcontext->screen_texture_pixels_wide)||
-		(dst_y_start < 0 || dst_y_start >= pcontext->screen_texture_pixels_high)
+		(dst_x_start < 0 || dst_x_start >= dst_width) ||
+		(dst_y_start < 0 || dst_y_start >= dst_height)
 		)
 	{
 		return;
 	}
 
 	/* clip */
-	offset_x = (dst_x_start + src_width) - (dst_x_start + pcontext->screen_texture_pixels_wide);
-	offset_y = (dst_y_start + src_height) - (dst_y_start + pcontext->screen_texture_pixels_high);
-	if (offset_x > 0)
+	offset_x = (dst_x_start + src_width) - dst_width;
+	offset_y = (dst_y_start + src_height) - dst_height;
+	if (offset_x > 0) {
 		src_width -= offset_x;
-	if (offset_y > 0)
+	}
+	if (offset_y > 0) {
 		src_height -= offset_y;
+	}
 
 	/* iterate through source rectangle */
 	/* copy to destination */
@@ -216,36 +223,230 @@ void rasterizer_copy_pixels(
 		for (y = 0; y < src_height; ++y)
 		{
 			src_loc = src_pixels + src_pitch * y + x * channels;
-			if (
-				((dst_x_start + x) < pcontext->screen_texture_pixels_wide) &&
-				((dst_y_start + y) < pcontext->screen_texture_pixels_high)
-				)
-			{
 				dst_loc = dst_pixels + 
 					dst_pitch * (dst_y_start + y) + 
 					(dst_x_start + x) * channels;
 				
-				if (alpha_blending_flag)
-				{
-					/* blend */
-					/* SourceColorAlpha * SourceColorBGR + (1 - SourceColorAlpha) * DestinationColorBGR */
-					src_alpha_uchar = *(src_loc + 3);
-					src_alpha = src_alpha_uchar * inv_255;
-					one_minus_src_alpha = 1.0 - src_alpha;
-					*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
-					*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
-					*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
-					*dst_loc = src_alpha_uchar;
-				}
-				else
-				{
-					*dst_loc++ = *src_loc++;
-					*dst_loc++ = *src_loc++;
-					*dst_loc++ = *src_loc++;
-					*dst_loc = *src_loc;
-				}
+			if (alpha_blending_flag) {
+				/* blend */
+				/* SourceColorAlpha * SourceColorBGR + (1 - SourceColorAlpha) * DestinationColorBGR */
+				src_alpha_uchar = *(src_loc + 3);
+				src_alpha = src_alpha_uchar * inv_255;
+				one_minus_src_alpha = 1.0 - src_alpha;
+				*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
+				*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
+				*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
+				*dst_loc = src_alpha_uchar;
+			}
+			else
+			{
+				*dst_loc++ = *src_loc++;
+				*dst_loc++ = *src_loc++;
+				*dst_loc++ = *src_loc++;
+				*dst_loc = *src_loc;
 			}
 		}
+	}
+}
+
+void rasterizer_copy_pixels_subimage(
+	int dst_x_start,
+	int dst_y_start,
+	int src_x_start,
+	int src_y_start,
+	int src_width,
+	int src_height,
+	int src_total_width,
+	int src_total_height,
+	unsigned char* src_pixels)
+{
+	unsigned char *dst_pixels = pcontext->screen_texture_pixels;
+	unsigned char *dst_loc, *src_loc;
+	int src_pitch = src_total_width * pcontext->screen_texture_channels;
+	int dst_total_width = pcontext->screen_texture_pixels_wide;
+	int dst_total_height = pcontext->screen_texture_pixels_high;
+	int dst_pitch = pcontext->screen_texture_pitch;
+	int channels = pcontext->screen_texture_channels;
+	int src_x, src_y, dst_x, dst_y;
+	int offset_x;
+	int offset_y;
+	int x, y;
+	double src_alpha;
+	double one_minus_src_alpha;
+	double inv_255 = 1.0 / 255.0;
+	unsigned char src_alpha_uchar;
+
+
+	/* sanity check dst_start and src_start coordinates */
+	if (
+		(dst_x_start < 0 || dst_x_start >= dst_total_width) ||
+		(dst_y_start < 0 || dst_y_start >= dst_total_height) ||
+		(src_x_start < 0 || src_x_start >= src_total_width) ||
+		(src_y_start < 0 || src_y_start >= src_total_height)
+		)
+	{
+		return;
+	}
+
+	/* clip at source */
+	offset_x = (src_x_start + src_width) - src_total_width;
+	offset_y = (src_y_start + src_height) - src_total_height;
+	if (offset_x > 0) {
+		src_width -= offset_x;
+	}
+	if (offset_y > 0) {
+		src_height -= offset_y;
+	}
+
+	/* clip at destination */
+	offset_x = (dst_x_start + src_width) - dst_total_width;
+	offset_y = (dst_y_start + src_height) - dst_total_height;
+	if (offset_x > 0) {
+		src_width -= offset_x;
+	}
+	if (offset_y > 0) {
+		src_height -= offset_y;
+	}
+
+	/* iterate through source rectangle */
+	/* copy to destination */
+	for (src_x = src_x_start, x = 0; src_x < (src_x_start + src_width); ++src_x, ++x)
+	{
+		for (src_y = src_y_start, y = 0; src_y < (src_y_start + src_height); ++src_y, ++y)
+		{
+			dst_x = dst_x_start + x;
+			dst_y = dst_y_start + y;
+			src_loc = src_pixels + src_pitch * src_y + src_x * channels;
+			dst_loc = dst_pixels + dst_pitch * dst_y + dst_x * channels;
+			if (alpha_blending_flag) {
+				/* blend */
+				/* SourceColorAlpha * SourceColorBGR + (1 - SourceColorAlpha) * DestinationColorBGR */
+				src_alpha_uchar = *(src_loc + 3);
+				src_alpha = src_alpha_uchar * inv_255;
+				one_minus_src_alpha = 1.0 - src_alpha;
+				*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
+				*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
+				*dst_loc++ = src_alpha**src_loc++ + *dst_loc * one_minus_src_alpha;
+				*dst_loc = src_alpha_uchar;
+			}
+			else {
+				*dst_loc++ = *src_loc++;
+				*dst_loc++ = *src_loc++;
+				*dst_loc++ = *src_loc++;
+				*dst_loc = *src_loc;
+			}
+		}
+	}
+}
+
+
+void rasterizer_copy_pixels_subimage_chromakey(
+	int dst_x_start,
+	int dst_y_start,
+	int src_x_start,
+	int src_y_start,
+	int src_width,
+	int src_height,
+	int src_total_width,
+	int src_total_height,
+	swr_color chroma_key,
+	unsigned char* src_pixels)
+{
+	unsigned char *dst_pixels = pcontext->screen_texture_pixels;
+	unsigned char *dst_loc, *src_loc;
+	int src_pitch = src_total_width * pcontext->screen_texture_channels;
+	int dst_total_width = pcontext->screen_texture_pixels_wide;
+	int dst_total_height = pcontext->screen_texture_pixels_high;
+	int dst_pitch = pcontext->screen_texture_pitch;
+	int channels = pcontext->screen_texture_channels;
+	int src_x, src_y,dst_x,dst_y;
+	int offset_x;
+	int offset_y;
+	int x, y;
+
+
+	/* sanity check dst_start and src_start coordinates */
+	if (
+		(dst_x_start < 0 || dst_x_start >= dst_total_width) ||
+		(dst_y_start < 0 || dst_y_start >= dst_total_height) ||
+		(src_x_start < 0 || src_x_start >= src_total_width) ||
+		(src_y_start < 0 || src_y_start >= src_total_height)
+		)
+	{
+		return;
+	}
+
+	/* clip at source */
+	offset_x = (src_x_start + src_width) - src_total_width;
+	offset_y = (src_y_start + src_height) - src_total_height;
+	if (offset_x > 0) {
+		src_width -= offset_x;
+	}
+
+	if (offset_y > 0) {
+		src_height -= offset_y;
+	}
+
+	/* clip at destination */
+	offset_x = (dst_x_start + src_width) - dst_total_width;
+	offset_y = (dst_y_start + src_height) - dst_total_height;
+	if (offset_x > 0) {
+		src_width -= offset_x;
+	}
+
+	if (offset_y > 0) {
+		src_height -= offset_y;
+	}
+
+
+	/* iterate through source rectangle */
+	/* copy to destination */
+	for (src_x = src_x_start, x = 0; src_x < (src_x_start+src_width); ++src_x, ++x)
+	{
+		for (src_y = src_y_start,y=0; src_y < (src_y_start + src_height); ++src_y,++y)
+		{
+			dst_x = dst_x_start + x;
+			dst_y = dst_y_start + y;
+			src_loc = src_pixels + src_pitch * src_y + src_x * channels;
+			dst_loc = dst_pixels + dst_pitch * dst_y + dst_x * channels;
+			if (chroma_key.b == *src_loc &&
+				chroma_key.g == *(src_loc + 1) &&
+				chroma_key.r == *(src_loc + 2)) 
+			{
+				dst_loc += 4;
+			}
+			else {
+				*dst_loc++ = *src_loc++;
+				*dst_loc++ = *src_loc++;
+				*dst_loc++ = *src_loc++;
+				*dst_loc = *src_loc;
+			}
+		}
+	}
+}
+
+
+void rasterizer_draw_text(swr_rfont *f, int x, int y, char* text)
+{
+	unsigned char c;
+	int i;
+	int maxCols = maxCols = f->font_image_width / f->font_cell_width;
+	unsigned char baseChar = f->font_base_char;
+	unsigned char charDiff;
+	int row, col;
+	swr_color chroma_key = { 255, 0, 255, 255 };
+	i = 0;
+	while ((c = text[i]) != '\0')
+	{
+		charDiff = c - baseChar;
+		row = charDiff / maxCols;
+		col = charDiff - (row*maxCols);
+		rasterizer_copy_pixels_subimage_chromakey
+			(x, y, col*f->font_cell_width, row*f->font_cell_height, 
+			f->font_cell_width, f->font_cell_height, f->font_image_width, 
+			f->font_image_height, chroma_key, f->font_image_pixels);
+		x += f->font_width[c];
+		++i;
 	}
 }
 
@@ -256,11 +457,16 @@ void rasterizer_clear()
 		pcontext->screen_texture_pixels_high;
 	int i;
 
+	unsigned char b = pcontext->clear_color.b, 
+		g = pcontext->clear_color.g, 
+		r = pcontext->clear_color.r, 
+		a = pcontext->clear_color.a;
+
 	for (i = 0; i < n; ++i) 
 	{
-		*loc++ = pcontext->clear_color.b;
-		*loc++ = pcontext->clear_color.g;
-		*loc++ = pcontext->clear_color.r;
-		*loc++ = pcontext->clear_color.a;
+		*loc++ = b;
+		*loc++ = g;
+		*loc++ = r;
+		*loc++ = a;
 	}
 }
